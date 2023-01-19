668efcfab856462e895ff629d33fcda0f7562881
seungsik <seungsik@9bit.co.kr>
2023-01-19 14:16:37 +0900

    StatelessWidget 사용하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    // 외워,,, MaterialApp -> Scaffold 기본 구조다..
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20.0
-            ),
-          ),
-        ),
+  runApp(MaterialApp(home: HomeScreen()));
+}
+
+// Hot Reload는 Widget build 함수를 실행한다.
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello world!!"),
       ),
-    )
-  );
+    );
+  }
 }
