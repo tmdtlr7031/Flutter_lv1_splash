095c26edaaae308cba692227af7956e51959505e
seungsik <seungsik@9bit.co.kr>
2023-01-19 14:30:29 +0900

    Hex Code 색상 사용, 시뮬레이터 debug 표시 숨기기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, // 시뮬레이터 우측상단 debug 표시 숨김
+    home: HomeScreen(),
+  ));
 }
 
 // Hot Reload는 Widget build 함수를 실행한다.
@@ -9,14 +12,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Color(0xFFF99231), // HEX CODE 넣기
       body: Column(
         // 화면 가장 위부터 children값을 표시해주는게 default
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // 자식 위젯 여러개 넣을 수 있음
           Image.asset('asset/img/logo.png'),
-          CircularProgressIndicator( // 로딩바
+          CircularProgressIndicator(
+            // 로딩바
             color: Colors.white,
           )
         ],
